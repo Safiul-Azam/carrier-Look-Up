fe857fb717d5acb652abdb3db44f896564192f1d
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-28 10:21:01 +0600

    toggle btn style done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-const nestedIcon = document.getElementById("nested-icon");
-const nestedMenuItem = document.getElementById("nested-menu-item");
-
+// TOGGLE FOR MENU PROFILE MENU AND SIDE MENU
 document.addEventListener("DOMContentLoaded", () => {
   const nestedIcon = document.getElementById("nested-icon");
   const nestedMenuItem = document.getElementById("nested-menu-item");
@@ -14,3 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {
     userMenu.classList.toggle("user-menu-toggle");
   });
 });
+// active menu set
+ document.querySelectorAll('a').forEach(item => {
+  item.addEventListener('click', event => {
+    if(item.class === "active"){
+    item.classList.remove("active");
+    }
+    item.classList.add("active");
+
+  });
+})
+
+//active sitting menu
